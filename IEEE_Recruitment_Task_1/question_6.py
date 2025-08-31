43ff536f14366b42e6e5b2cd5c17550fd5263a3a
mathananya <40272810+mathananya@users.noreply.github.com>
2025-08-31 13:39:43 +0530

    Updated question 6

---
+++ IEEE_Recruitment_Task_1/question_6.py
@@ -0,0 +1,40 @@
+# THE FOLLOWING CODE IS WEBSCRAPING THE DATA FROM THE WEBSITE
+# SKIP TO LINE 34 FOR CODE RELATED TO QUESTION
+
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www.bitsadmission.com/fd/BITSAT_cutOffs.html?yr=2024-2025')
+
+soup = BeautifulSoup(response.content,'html.parser')
+
+yeardata = soup.find('div',id='2024-2025')
+
+campi = yeardata.find_all('table')      #iBelieveInLatin
+
+for campus in range(len(campi)):
+    campi[campus] = campi[campus].find_all('tr')
+    for branch in range(len(campi[campus])):
+        campi[campus][branch] = campi[campus][branch].find_all('span')
+        for data in range(len(campi[campus][branch])):
+            campi[campus][branch][data] = campi[campus][branch][data].get_text()
+
+
+theActualList = campi[0]+campi[1]+campi[2]
+l = []
+for eachData in theActualList:
+    try:
+        if eachData[1] in ['B.E. Computer Science','B.E. Chemical','M.Sc. Biological Sciences','M.Sc. Economics']:
+            l.append(tuple(eachData[0:3]))
+    except:
+        pass
+
+# CONVERSION OF LIST TO DICTIONARY AS FOLLOWS
+
+d = dict()
+for i in l:
+    if i[0] not in d:
+        d[i[0]] = {i[1]:i[2]}
+    else:
+        d[i[0]][i[1]] = i[2]
+print(d)
